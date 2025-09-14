8ed74a226ecb3d27b850ebf894277513ef550d03
ganesh-legend <ganeshpawar06969@gmail.com>
2025-09-14 20:14:31 +0530

    code addes

---
+++ src/main/java/com/example/NoDatabaseSpringApp/NoDatabaseSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.NoDatabaseSpringApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoDatabaseSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoDatabaseSpringAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/NoDatabaseSpringApp/NoDatabaseSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NoDatabaseSpringApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoDatabaseSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
